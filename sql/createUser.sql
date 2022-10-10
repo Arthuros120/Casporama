c32bd56b6bc2d32b275bbed8dfac4ce67f74bdc0
Hamelin Arthur <arthuros222@gmail.com>
2022-10-10 18:12:48 +0200

    update4 user sql file

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -4,9 +4,9 @@ DROP USER IF EXISTS 'maximef'@'%';
 DROP USER IF EXISTS 'titouan'@'%';
 
 CREATE USER 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
-CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'maxime123';
-CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'maxime123';
-CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'titouan123';
+CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
+CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
+CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
 
 GRANT ALL PRIVILEGES ON *.* TO 'arthur' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'maximes' WITH GRANT OPTION;
