ace545b9c8b673dc146c7b24816733998940ae5d
Hamelin Arthur <arthuros222@gmail.com>
2022-10-10 22:07:23 +0200

    update7 user sql file

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -10,14 +10,17 @@ DROP USER IF EXISTS 'maximef'@'%';
 DROP USER IF EXISTS 'titouan'@'%';
 
 
-CREATE USER 'arthur'@'90.93.179.210' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
-CREATE USER 'maximes'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
-CREATE USER 'maximef'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
-CREATE USER 'titouan'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
+CREATE USER 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
+CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
+CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
+CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
+CREATE USER 'pma'@'%' IDENTIFIED WITH mysql_native_password BY  'PmaServer2022$';
 
-GRANT ALL PRIVILEGES ON *.* TO 'arthur' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'maximes' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'maximef' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'titouan' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON *.* TO 'arthur'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'maximes'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'maximef'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'titouan'@'%';
+GRANT ALL PRIVILEGES ON phpmyadmin .* TO 'pma'@'%';
 
 FLUSH PRIVILEGES;
