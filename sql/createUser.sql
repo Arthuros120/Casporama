3a0f31482f0e751e6daa51b4dc4a70a35e3b2e41
Hamelin Arthur <arthuros222@gmail.com>
2022-10-10 22:46:32 +0200

    update6 user sql file

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -9,15 +9,14 @@ DROP USER IF EXISTS 'maximes'@'%';
 DROP USER IF EXISTS 'maximef'@'%';
 DROP USER IF EXISTS 'titouan'@'%';
 
-DROP DATABASE IF EXISTS 'db_itconnect';
+DROP DATABASE 'db_itconnect';
 
-CREATE DATABASE IF EXISTS 'db_itconnect';
+CREATE DATABASE 'db_itconnect';
 
 CREATE USER 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
 CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
 CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
 CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
-CREATE USER 'pma'@'%' IDENTIFIED WITH mysql_native_password BY  'PmaServer2022$';
 
 GRANT ALL PRIVILEGES ON db_itconnect.* TO 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$' WITH GRANT OPTION;
 
@@ -26,6 +25,5 @@ GRANT ALL PRIVILEGES ON *.* TO 'arthur'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'maximes'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'maximef'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'titouan'@'%';
-GRANT ALL PRIVILEGES ON phpmyadmin .* TO 'pma'@'%';
 
 FLUSH PRIVILEGES;
