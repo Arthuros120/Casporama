d5ce4713a1bb10e78d48bf7c9ef3b313c89e9325
Hamelin Arthur <arthuros222@gmail.com>
2022-10-10 18:42:58 +0200

    update5 user sql file

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -1,14 +1,16 @@
-DROP USER IF EXISTS 'arthur'@'%';
-DROP USER IF EXISTS 'maximes'@'%';
-DROP USER IF EXISTS 'maximef'@'%';
-DROP USER IF EXISTS 'titouan'@'%';
+DROP USER IF EXISTS 'arthur'@'0.0.0.0';
+DROP USER IF EXISTS 'maximes'@'0.0.0.0';
+DROP USER IF EXISTS 'maximef'@'0.0.0.0';
+DROP USER IF EXISTS 'titouan'@'0.0.0.0';
 
-CREATE USER 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
-CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
-CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
-CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
+CREATE USER 'arthur'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
+CREATE USER 'maximes'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
+CREATE USER 'maximef'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
+CREATE USER 'titouan'@'0.0.0.0' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
 
 GRANT ALL PRIVILEGES ON *.* TO 'arthur' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'maximes' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'maximef' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'titouan' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
