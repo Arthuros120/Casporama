f74fbfe651f5bce8d327c0d5e9ee8024035e725d
Hamelin Arthur <arthuros222@gmail.com>
2022-10-11 13:50:03 +0200

    update 9 user sql file

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -9,16 +9,11 @@ DROP USER IF EXISTS 'maximes'@'%';
 DROP USER IF EXISTS 'maximef'@'%';
 DROP USER IF EXISTS 'titouan'@'%';
 
-DROP DATABASE db_itconnect;
-
-CREATE DATABASE db_itconnect;
 
 CREATE USER 'arthur'@'%' IDENTIFIED WITH mysql_native_password BY 'ArthurHamelin123$';
 CREATE USER 'maximes'@'%' IDENTIFIED WITH mysql_native_password BY  'MaximeSantos123$';
 CREATE USER 'maximef'@'%' IDENTIFIED WITH mysql_native_password BY 'MaximeFranco123$';
 CREATE USER 'titouan'@'%' IDENTIFIED WITH mysql_native_password BY 'TitouanGautier123$';
-
-GRANT ALL PRIVILEGES ON db_itconnect.* TO 'arthur'@'%' IDENTIFIED BY 'ArthurHamelin123$' WITH GRANT OPTION;
 
 
 GRANT ALL PRIVILEGES ON *.* TO 'arthur'@'%';
