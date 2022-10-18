227a51dd5f90fa953a7838acd82a75d2963e7d7d
you@example.com <you@example.com>
2022-10-18 09:54:16 +0000

    front home page

--- Casporama/application/views/home.php
+++ Casporama/application/views/home.php
@@ -1,6 +1,35 @@
-<a href="/Shop/home/foot"><h1>Foot</h1></a>
-<a href="/Shop/home/volley"><h1>Volley Ball</h1></a>
-<a href="/Shop/home/bad"><h1>Badminton</h1></a>
-<a href="/Shop/home/mma"><h1>Mixed Martial Art</h1></a>
-
-<img src="<?php echo base_url(); ?>static/image/img.jpg" alt="" />
+<div class="home">
+    <ul class="grid">
+        <li class="card foot">
+            <div class="incard">
+                <a href="/Shop/home/foot">
+                    <h1>Football</h1>
+                </a>
+            </div>
+        </li>
+        <li class="card volley">
+            <div class="incard">
+                <a href="/Shop/home/volley">
+                    <h1>Volley Ball</h1>
+                </a>
+            </div>
+            
+        </li>
+        <li class="card bad">
+            <div class="incard">
+                <a href="/Shop/home/bad">
+                    <h1>Badminton</h1>
+                </a>
+            </div>
+            
+        </li>
+        <li class="card mma">
+            <div class="incard">
+                <a href="/Shop/home/mma">
+                    <h1>Mixed Martial Art</h1>
+                </a>
+            </div>
+            
+        </li>
+    </ul>
+</div>
--- Casporama/application/views/templates/head.php
+++ Casporama/application/views/templates/head.php
@@ -1,1 +1,3 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>static/base.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>static/css/base.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>static/css/home_content.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>static/css/button.css">
