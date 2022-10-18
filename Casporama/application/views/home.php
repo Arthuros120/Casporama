0f7297919bb89b293a4f709f01336871d8f0b0be
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-10-18 08:48:44 +0200

    fix front

--- Casporama/application/views/home.php
+++ Casporama/application/views/home.php
@@ -1,6 +1,15 @@
-<a href="/Shop/home/foot"><h1>Foot</h1></a>
-<a href="/Shop/home/volley"><h1>Volley Ball</h1></a>
-<a href="/Shop/home/bad"><h1>Badminton</h1></a>
-<a href="/Shop/home/mma"><h1>Mixed Martial Art</h1></a>
+
+
+
+
 
 <img src="<?php echo base_url(); ?>static/image/img.jpg" alt="" />
+
+<div>
+    <ul class="grid">
+        <li class="card"><a href="/Shop/home/foot"><h1>Foot</h1></a></li>
+        <li class="card"><a href="/Shop/home/volley"><h1>Volley Ball</h1></a></li>
+        <li class="card"><a href="/Shop/home/bad"><h1>Badminton</h1></a></li>
+        <li class="card"><a href="/Shop/home/mma"><h1>Mixed Martial Art</h1></a></li>
+    </ul>
+</div>
