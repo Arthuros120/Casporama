7db092278816ec0ca0a995bb86155db1419541a9
you@example.com <you@example.com>
2022-10-18 12:28:54 +0000

    80% home page

--- Casporama/application/views/home/home.php
+++ Casporama/application/views/home/home.php
@@ -1,35 +1,57 @@
 <div class="home">
     <ul class="grid">
+        
         <li class="card foot">
             <div class="incard">
                 <a href="/Shop/home/Football">
-                    <h1>Football</h1>
+                    <!-- <img class="home_img" src="<?php echo base_url() ?>static/image/home_foot.png"> -->
                 </a>
+                <div class="bg_title"></div>
+                <div class="home_title">
+                    <h3>Football</h3>
+                </div>
+                <a class="link2" href="/Shop/home/Football"></a>
             </div>
         </li>
+
+        <li class="card bad">
+            <div class="incard">
+                <a href="/Shop/home/Badminton">
+                    <img class="home_img" src="<?php echo base_url() ?>static/image/home_bad.png">
+                </a>
+                <div class="bg_title"></div>
+                <div class="home_title">
+                    <h3>Badminton</h3>
+                </div>
+                <a class="link2" href="/Shop/home/Badminton"></a>
+            </div> 
+        </li>
+
         <li class="card volley">
             <div class="incard">
                 <a href="/Shop/home/Volleyball">
                     <img class="home_img" src="<?php echo base_url() ?>static/image/home_volley.png">
                 </a>
+                <div class="bg_title"></div>
+                <div class="home_title">
+                    <h3>Volleyball</h3>
+                </div>
+                <a class="link2" href="/Shop/home/Volleyball"></a>
             </div>
-            
         </li>
-        <li class="card bad">
-            <div class="incard">
-                <a href="/Shop/home/Badminton">
-                    <h1>Badminton</h1>
-                </a>
-            </div>
-            
-        </li>
+
         <li class="card mma">
             <div class="incard">
-                <a href="/Shop/home/Art_martiaux">
-                    <h1>Mixed Martial Art</h1>
+                <a class="link1" href="/Shop/home/Art_martiaux">
+                    <img class="home_img" src="<?php echo base_url() ?>static/image/home_mma.png">
                 </a>
+                <div class="bg_title"></div>
+                <div class="home_title">
+                    <h3>Arts-Martiaux</h3>
+                </div>
+                <a class="link2" href="/Shop/home/Art_martiaux"></a>                
             </div>
-            
         </li>
+
     </ul>
 </div>
--- Casporama/application/views/shop/global/header.php
+++ Casporama/application/views/shop/global/header.php
@@ -5,4 +5,22 @@
     <a href="<?= base_url("/Shop/product/" . $sport . "/Chaussure") ?>"><h2>Chaussures</h2></a>
     <a href="<?= base_url("/Shop/product/" . $sport . "/Equipement") ?>"><h2>Equipement</h2></a>
 
+    <!-- <nav class="nav">
+            <div class="left">
+                <img class="logo" src="img/Logo_casporama-removebg-preview 1.png">
+            </div>
+            <div class="right">
+                <ul class="list_nav">
+                    <li class="text">Vêtements</li>
+                    <li class="text">Chaussures</li>
+                    <li class="text">Equipements</li>
+                    <div class="list_line">
+                        <hr>
+                    </div>
+                    <li class="icon list_icon"><img src="img/shop.png"></li>
+                    <li class="icon list_icon"><img src="img/Account.png"></li>
+                </ul>
+            </div>
+        </nav> -->
+
 </header>
