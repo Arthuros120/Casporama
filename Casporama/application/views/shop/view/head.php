c8130e1fb08ce57d8184801c603aa087e524674a
you@example.com <you@example.com>
2022-10-24 12:50:55 +0000

    fix front homepages and product bug

--- Casporama/application/views/shop/view/head.php
+++ Casporama/application/views/shop/view/head.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>static/css/header_global.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>static/css/button.css">
 <link rel="stylesheet" type="text/css" href="<?= base_url("/static/css/" . $sport . "/colors.css") ?>">
-<link rel="stylesheet" href="<?php echo base_url()?>static/css/product_global.css">
+<link rel="stylesheet" href="<?php echo base_url()?>static/css/view_global.css">
