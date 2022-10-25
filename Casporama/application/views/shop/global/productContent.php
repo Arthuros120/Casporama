86cf9b21e237c96daa9ea4d3be1b86549ce1f7ab
you@example.com <you@example.com>
2022-10-25 10:19:50 +0000

    frontend: product view advance

--- Casporama/application/views/shop/global/productContent.php
+++ Casporama/application/views/shop/global/productContent.php
@@ -1,12 +1,31 @@
 <!-- shop/global/productContent -->
 
 <div class="productContent">
-    <ul>
+    <ul class="grid_product">
         <li class="card">
-
+            <ul class="grid_img">
+                <li class="second_img">
+                    <?php for ($i = 1; $i < 2; $i++) : ?>
+                    <div>
+                        <img src="<?= base_url($product -> get_images()[$i]) ?>">
+                    </div>
+                    <?php endfor?>
+                </li>
+                <li class="main_img">
+                    <div>
+                        <img src=<?= $product->get_cover() ?>>
+                    </div>
+                </li>
+            </ul>
         </li>
         <li class="card">
-            
+            <div class="brand">
+                <h2><?= $product -> get_brand() ?></h2>
+            </div>
+            <div class="title">
+                <h1><?= $product -> get_name() ?></h1>
+            </div>
+
         </li>
     </ul>
 </div>
--- Casporama/application/views/shop/view/head.php
+++ Casporama/application/views/shop/view/head.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>static/css/button.css">
 <link rel="stylesheet" type="text/css" href="<?= base_url("/static/css/" . $sport . "/colors.css") ?>">
 <link rel="stylesheet" href="<?php echo base_url()?>static/css/view_global.css">
+<link rel="stylesheet" href="<?php echo base_url()?>static/css/product_content.css">
