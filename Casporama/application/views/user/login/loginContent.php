7e6ee1ed0e76a4a439448716da95713b8b1a147d
you@example.com <you@example.com>
2022-11-07 16:40:43 +0000

    frontend work

--- Casporama/application/views/user/login/loginContent.php
+++ Casporama/application/views/user/login/loginContent.php
@@ -3,41 +3,26 @@
 <?php echo form_open('User/login'); ?>
 
 <div class="login-card">
-    <a href="<?= base_url() ?>">
-
-        <img src="<?= base_url() . "static/image/icon/casporama.svg" ?>" alt="Casporama" />
-
-    </a>
+    <a href="<?= base_url() ?>"><img src="<?= base_url() . "static/image/icon/casporama.svg" ?>" alt="Casporama" /></a>
 
     <h2>Connexion</h2>
 
     <div class="login-form">
-
         <input type="text" name="login" placeholder="Login ou Email" />
-
         <input type="password" name="password" placeholder="Mots de passe" />
+
         <a href="<?= base_url("/User/recoverPass") ?>">Mots de passe oublié ?</a>
-
-        <h4>
-
-            Rester connecté :
-            <input id="checkBox" type="checkbox" name="conPersistance" />
-
-        </h4>
-
+        
+        <h4>Rester connecté :<input id="checkBox" type="checkbox" name="conPersistance" /></h4>
         <button type="submit">Se connecter</button>
         <a id="registerLink" href="<?= base_url("/User/register") ?>">Créer un compte</a>
-
     </div>
 </div>
 
-<div class="error" <?php
+    <?php if ($error == null) {
+            echo "hidden";
+        }?>>
+    <?= $error ?>
 
-                    if ($error == null) {
-                        echo "hidden";
-                    }
-
-                    ?>><?= $error ?>
-</div>
 
 <!-- user/login/loginContent -->
