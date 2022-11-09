7cc66593dcecf2b08f2d9f46e6d0bbb689bd25ff
Maxime SANTOS PEREIRA <E215890N@univ-nantes.fr>
2022-11-09 08:13:44 +0100

    Debut convertion en mariadb

--- sql/table.sql
+++ sql/table.sql
@@ -7,7 +7,9 @@ CREATE TABLE IF NOT EXISTS utilisateur (
     password VARCHAR(255) NOT NULL,
     salt VARCHAR(45) NOT NULL unique,
     cookieId VARCHAR(45),
-    status VARCHAR(20) not null check (status in ('Administrateur','Client','Caspor')),
+    status VARCHAR(20) not null,
+    constraint status_not_valid 
+        check(status in ('Administrateur','Client','Caspor')),
     PRIMARY KEY (id)
 );
 
@@ -16,10 +18,10 @@ CREATE TABLE IF NOT EXISTS coordonnees (
     prenom VARCHAR(255),
     nom VARCHAR(255),
     mail VARCHAR(255) unique,
-    mobile varchar(10)  unique,
+    mobile varchar(10) unique,
     fixe varchar(10),
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES utilisateur(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES utilisateur(id)
 );
 
 CREATE TABLE IF NOT EXISTS localisation (
@@ -30,54 +32,60 @@ CREATE TABLE IF NOT EXISTS localisation (
     ville VARCHAR(255) not null,
     departement VARCHAR(255) not null,
     pays VARCHAR(255) not null,
-    PRIMARY KEY (idadresse),
-    FOREIGN KEY (id) REFERENCES utilisateur(id)
+    PRIMARY KEY(idadresse),
+    FOREIGN KEY(id) REFERENCES utilisateur(id)
 );
 
 CREATE TABLE IF NOT EXISTS sport (
     nusport INTEGER NOT NULL,
     nom VARCHAR(20) NOT NULL UNIQUE,
-    PRIMARY KEY (nusport)
+    PRIMARY KEY(nusport)
 );
 
 
 create table if not exists produit (
     idproduit integer not null,
-    reference integer not null,
-    type varchar(15) not null check ( type in ('Vêtement','Chaussure','Equipement') ),
+    type varchar(15) not null,
     nusport integer not null,
     marque VARCHAR(255) not null,
     nom VARCHAR(255) NOT NULL UNIQUE,
-    genre VARCHAR(5) NOT NULL check ( genre in ('Homme','Femme','Mixte') ),
+    genre VARCHAR(5) NOT NULL,
     prix float not null,
     description text not null,
     image VARCHAR(255),
-    PRIMARY KEY (idproduit),
-    FOREIGN KEY (nusport) REFERENCES sport(nusport)
+    PRIMARY KEY(idproduit),
+    FOREIGN KEY(nusport) REFERENCES sport(nusport),
+    constraint type_not_valid 
+        check( type in ('Vêtement','Chaussure','Equipement') ),
+    constraint gender_not_valid 
+        check( genre in ('Homme','Femme','Mixte'))
 );
 
 CREATE TABLE IF NOT EXISTS catalogue (
     id INTEGER NOT NULL,
     nuproduit integer not null,
+    reference integer not null,
     couleur varchar(20),
     taille varchar(3),
     quantite integer not null default 0,
-    PRIMARY KEY (id),
-    foreign key (nuproduit) references produit(idproduit)
+    PRIMARY KEY(id),
+    foreign key(nuproduit) references produit(idproduit)
 );
 
 create table if not exists commande (
     idcommande integer not null,
-    datecommande varchar(10) not null,
-    idproduit integer not null,
-    quantite integer not null,
+    datecommande date not null,
+    idproduit varchar(255) not null,
+    quantite varchar(255) not null,
     idclient integer not null,
     idadresse int not null,
-    etat varchar(15) not null check ( etat in ('En cours','En preparation','Terminer') ),
-    primary key (idcommande),
-    foreign key (idadresse) references localisation(idadresse),
-    foreign key (idclient) references utilisateur(id),
-    foreign key (idproduit) references produit(idproduit)
+    etat varchar(15) not null,
+    primary key(idcommande),
+    foreign key(idadresse) references localisation(idadresse),
+    foreign key(idclient) references utilisateur(id),
+    foreign key(idproduit) references produit(idproduit),
+    constraint status_not_valid 
+        check(etat in ('Non preparer','En preparation','Preparer','Expedier'))
 );
 
 -- Ajout de la table captcha pour la gestion des captcha
