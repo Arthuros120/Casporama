c0fb6f18162aada7f2579edf23cb9e1fbcfccee4
you@example.com <you@example.com>
2022-11-09 08:36:56 +0000

    work on page 404

--- Casporama/application/views/errors/html/error_404.php
+++ Casporama/application/views/errors/html/error_404.php
@@ -1,17 +1,25 @@
 <?php
 
-defined('BASEPATH') || exit('No direct script access allowed');
-
-//TODO: Prévoir une redirection automatique au bout de 10 secondes
+    defined('BASEPATH') || exit('No direct script access allowed');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="https://casporama.live/static/css/fonts.css">
+<link rel="stylesheet" href="https://casporama.live/static/css/global/color.css">
 
 <head>
     <meta charset="utf-8">
     <title>404 Page non trouvée</title>
     <style type="text/css">
+        * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+
+            font-family: Poppins, Arial, sans-serif;
+        }
+
         ::selection {
             background-color: #E13300;
             color: white;
@@ -24,60 +32,70 @@ defined('BASEPATH') || exit('No direct script access allowed');
 
         body {
             background-color: #fff;
-            margin: 40px;
-            font: 13px/20px normal Helvetica, Arial, sans-serif;
-            color: #4F5155;
-        }
-
-        a {
-            color: #003399;
-            background-color: transparent;
-            font-weight: normal;
-        }
-
-        h1 {
-            color: #444;
-            background-color: transparent;
-            border-bottom: 1px solid #D0D0D0;
-            font-size: 19px;
-            font-weight: normal;
-            margin: 0 0 14px 0;
-            padding: 14px 15px 10px 15px;
-        }
-
-        code {
-            font-family: Consolas, Monaco, Courier New, Courier, monospace;
-            font-size: 12px;
-            background-color: #f9f9f9;
-            border: 1px solid #D0D0D0;
-            color: #002166;
-            display: block;
-            margin: 14px 0 14px 0;
-            padding: 12px 10px 12px 10px;
-        }
-
-        #container {
-            margin: 10px;
-            border: 1px solid #D0D0D0;
-            box-shadow: 0 0 8px #D0D0D0;
         }
 
         p {
             margin: 12px 15px 12px 15px;
         }
+
+        .error_content {
+            width: 100vw;
+            height: 100vh;
+        }
+
+        .logo {
+            width: 100vw;
+            height: 10vh;
+
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+
+            padding-left: 2vw;
+        }
+
+        .content {
+            width: 100%;
+            height: 90vh;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            padding-top: 10vh;
+        }
+
+        .content > h1 {
+            font-size: 12vw;
+            font-weight: 600;
+
+        }
+
+        .vert {
+            color: var(--green2)
+        }
+
     </style>
 
 </head>
 
 <body>
 
-    <div id="container">
+    <div  class="error_content" id="container">
 
-        <h1><?php echo $heading; ?></h1>
 
-        <?php echo $message; ?>
+        <div class="logo">
+            <img src="https://casporama.live/static/image/icon/casporama.svg">
+        </div>
 
-        <h3>Vous allez être automatique redirigé vers la page d'accueil dans <span id='time'>10 secondes</span></h3>
+        <div class="content">
+            <h1><span class="vert">O</span><span class="bleu">o</span><span class="jaune">p</span><span class="rouge">s</span></h1>
+            <h2><?php echo $heading; ?></h2>
+            <h2><?php echo $message; ?></h2>
+            <h3>Vous allez être automatique redirigé vers la page d'accueil dans <span id='time'>10 secondes</span></h3>
+        </div>
+
+        
 
     </div>
 
