46b20ef223246b20467ee185363cc94051d55100
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-11-10 14:55:43 +0100

    register page almost finished

--- Casporama/application/views/user/register/content.php
+++ Casporama/application/views/user/register/content.php
@@ -12,43 +12,41 @@
                 alt="Casporama" />
             </a>
         </div>
+        <div class="title">
+            <h2>Inscription</h2>
+        </div>
         <div class="form">
             <div class="name">
-                <h3>Nom</h3>
-                <div class="inName">
-                    <input class="input" type="text" name="Surname" placeholder="Name" />
-                    <input class="input" type="text" name="Firstname" placeholder="First Name" />
-                </div>
+                <input class="input" type="text" name="Surname" placeholder="Name" />
+                <input class="input" type="text" name="Firstname" placeholder="First Name" />
             </div>
             <div class="login">
                 <input class="input" type="text" name="login" placeholder="Votre login" />
             </div>
             <div class="email">
                 <input class="input" type="email" name="email" placeholder="Votre email" />
-                <input class="input" type="email" name="emailConf" autocomplete="off" placeholder="Comfirmation de l'email" />
             </div>
             <div class="pass">
                 <input class="input" type="password" name="password" placeholder="Mots de passe" />
                 <input class="input" type="password" name="passConf" autocomplete="off" placeholder="Comfiramtion du Mots de passe" />
             </div>
+            <?php if ($captcha_form) { ?>
+                <input class="input" name="not_robot" id="not_robot" type="text" placeholder="Rentrer le captcha" />
+                <div class="captcha">
+                    <p><?php echo $captcha_html; ?></p>
+                </div>
+            <?php } ?>
+            <button class="submit" type="submit">Suivant</button>
         </div>
 
     </div>
 
 </div>
 
-<!-- 
-
-<h2>Creation de compte</h2>
 
 
 
-<?php if ($captcha_form) { ?>
-    <p>Captcha : <input name="not_robot" id="not_robot" type="text" /></p>
-    <p><?php echo $captcha_html; ?></p>
-<?php } ?>
 
-<button type="submit">Suivant</button>
 
 <div class="error" 
     <?php
@@ -56,6 +54,6 @@
             echo "hidden";
         }?>>
     <?= $error ?>
-</div> -->
+</div>
 
 <!-- user/register/Content -->
