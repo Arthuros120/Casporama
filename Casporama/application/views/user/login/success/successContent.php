b80b807b9e6e22dc3ce630684285128ce7d156f0
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-11-18 10:07:19 +0100

    loading nimation end

--- Casporama/application/views/user/login/success/successContent.php
+++ Casporama/application/views/user/login/success/successContent.php
@@ -1,20 +1,17 @@
 <!-- user/login/success/successContent -->
 
 <div class="success_content">
-    <div class="success_title">
-        <h1> Bonjour <?= $user->getLogin() ?></h1>
-    </div>
     <div class="success_loading">
         <h1 class="text-pop-up-top loading_text">
-            <span class="C">C</span>
-            <span class="A">A</span>
+            C
+            A
             <span class="S">S</span>
             <span class="P">P</span>
             <span class="O">O</span>
             <span class="R">R</span>
-            <span class="A">A</span>
-            <span class="M">M</span>
-            <span class="A">A</span>
+            A
+            M
+            A
         </h1>
         <h2>Chargment...</h2>
     </div>
