090571ff68f52d4602948b21bad21633a45c815c
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-12-08 14:09:21 +0100

    cerify link

--- Casporama/application/views/user/verify/sendVerify/content.php
+++ Casporama/application/views/user/verify/sendVerify/content.php
@@ -1,13 +1,26 @@
-<!-- user/verify/errNotVerif/content -->
+<!-- user/verify/sendVerify/content -->
 
-<h1> Un email vient de vous être envoyé</h1>
+<div class="verify_account_content">
 
-<p>Vous avez reçu un email de confirmation d'inscription à l'email suivant: <?=$user->getCoordonnees()->getEmail()?></p>
+    <div class="verify_title">
+        <div class="verify_title_main">        
+            <h1> Un email vient de vous être envoyé</h1>
+        </div>
+        <p>Vous avez reçu un email de confirmation d'inscription à l'email suivant: <?=$user->getCoordonnees()->getEmail()?></p>
+    </div>
 
-<p> Veuillez cliquer sur le lien de confirmation et entrer le code pour activer votre compte. </p>
+    <div class="verify_content">
+        <p> Veuillez cliquer sur le lien de confirmation et entrer le code pour activer votre compte. </p>
+        <p> Si vous n'avez pas reçu cet email, vous pouvez le demander à nouveau en cliquant sur le lien ci-dessous. </p>
+        <p> <a href="<?php echo site_url('User/sendVerify'); ?>"> Renvoyer l'email de confirmation </a> </p>
+    </div>
 
-<p> Si vous n'avez pas reçu cet email, vous pouvez le demander à nouveau en cliquant sur le lien ci-dessous. </p>
+</div>
 
-<p> <a href="<?php echo site_url('User/sendVerify'); ?>"> Renvoyer l'email de confirmation </a> </p>
 
-<!-- user/verify/errNotVerif/con<tent -->
+
+
+
+
+
+<!-- user/verify/sendVerify/content -->
---
+++ Casporama/application/views/user/verify/sendVerify/head.php
@@ -0,0 +1,6 @@
+<!-- user/verify/sendVerify/head -->
+
+<link rel="stylesheet" href="<?php echo base_url() ?>static/css/user/verify/verify_content.css">
+
+<title>Vérification</title>
+<!-- user/verify/sendVerify/head -->
