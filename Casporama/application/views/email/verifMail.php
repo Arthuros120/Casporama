0b06404eaba7c044d423ce8ccbfbf33d8a458364
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-12-08 14:52:08 +0100

    mail

--- Casporama/application/views/email/verifMail.php
+++ Casporama/application/views/email/verifMail.php
@@ -6,20 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
+<style>
+
+    .mail_content {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
+
 <body>
-
-<h1>Bonjour Nom Prénom</h1>
-
-<p> Il est important de vérifier votre adresse mail pour activer votre compte</p>
-<p> Voici le lien et le code de vérification pour activer vortre compte :</p>
-<p> Lien de vérification : <a href="lien">Cliquez ici</a></p>
-<p> Code de vérification : CODE</p>
-<p> Ce code est valable 6 heures</p>
-<p> Le lien expire le : DATE</p>
-<p> Si vous n'avez pas demandé à créer un compte sur Casporama, veuillez ignorer ce mail</p>
-
-
+    <table style="width:100%;">
+        <thead>
+            <th><img src="https://casporama.live/static/image/casporama.png"></th>
+        </thead>
+    </table>
 </body>
 </html>
 
 <!-- email/verifMail -->
+
+<!-- <h1>Bonjour Nom Prénom</h1>
+
+    <p> Il est important de vérifier votre adresse mail pour activer votre compte</p>
+    <p> Voici le lien et le code de vérification pour activer vortre compte :</p>
+    <p> Lien de vérification : <a href="lien">Cliquez ici</a></p>
+    <p> Code de vérification : CODE</p>
+    <p> Ce code est valable 6 heures</p>
+    <p> Le lien expire le : DATE</p>
+    <p> Si vous n'avez pas demandé à créer un compte sur Casporama, veuillez ignorer ce mail</p> -->
