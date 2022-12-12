f2d39d52fee061a690a7a013ca1376ca34967f9d
E217657J <titouan.gautier@etu.univ-nantes.fr>
2022-12-12 09:46:36 +0100

    mail

--- Casporama/application/views/email/verifMail.php
+++ Casporama/application/views/email/verifMail.php
@@ -7,22 +7,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<style>
-
-    .mail_content {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-    }
-
-</style>
-
-<body>
-    <table style="width:100%;">
-        <thead>
-            <th><img src="https://casporama.live/static/image/casporama.png"></th>
-        </thead>
-    </table>
+<body style="margin:0;padding:0;">
+    <table role="presentation" style="width:100%;border-collapse:collapse;border-spacing:0;background:#ffffff;">
+        <tr>
+            <td align="center" style="padding:0;">
+                <table role="presentation" style="width:50%;border-collapse:collapse;border-spacing:0;text-align:left;">
+                    <tr>
+                        <td align="center">
+                            <img src="https://casporama.live/static/image/casporama.png" alt="Logo" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding:0; line-height:1;">
+                            <h1 style="font-size:2em ;">Bonjour Nom Prenom</h1>
+                            <p>Il est important de vérifier votre adresse mail pour activer votre compte</p>
+                            <p>Voici le lien et le code de vérification pour activer vortre compte :</p>
+                        </td>
+                    </tr>
+                    <tr align="center">
+                        <table style="width: 30%;">
+                            <tr>
+                                <td align="center" style="padding:10 ; background:black">
+                                    <a style="text-decoration:none ;" href="/"><p style="color:white; font-weight:600;">Valider</p></a>
+                                </td>
+                            </tr>
+                        </table>
+                    </tr>
+                    <tr>
+                        <td align="center" style="padding:25;">
+                            <h2>123456</h2>
+                        </td>
+                    </tr>
+                    <tr align="center">
+                       <td style="line-height:1 ;" align="center">
+                           <p style="color:lightgrey;"> Ce code est valable 6 heures
+                           <p style="color:lightgrey;">Le lien expire le : DATE</p>
+                           <p style="color:lightgrey;">Si vous n'avez pas demandé à créer un compte sur Casporama, veuillez ignorer ce mail</p>
+                           </p>
+                       </td>
+                   </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
 
@@ -30,8 +57,8 @@
 
 <!-- <h1>Bonjour Nom Prénom</h1>
 
-    <p> Il est important de vérifier votre adresse mail pour activer votre compte</p>
-    <p> Voici le lien et le code de vérification pour activer vortre compte :</p>
+    <p> </p>
+    <p> </p>
     <p> Lien de vérification : <a href="lien">Cliquez ici</a></p>
     <p> Code de vérification : CODE</p>
     <p> Ce code est valable 6 heures</p>
