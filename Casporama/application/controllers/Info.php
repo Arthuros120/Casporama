eea14bdf60609efc52bf9e14a0ccbe6653f2a522
Luna MANSON <9223-E21A901C@users.noreply.gitlab.univ-nantes.fr>
2022-12-12 10:45:38 +0100

    Test informations 3

--- Casporama/application/controllers/Info.php
+++ Casporama/application/controllers/Info.php
@@ -3,14 +3,16 @@
 class Info extends CI_Controller
 {
 
+
     public function __construct()
     {
-
-
+        parent::__construct();
     }
+
 
     public function index()
     {
+        $this->output->enable_profiler(true);
         $this->LoaderView->load("Test/information/inforgpd");
     }
 
