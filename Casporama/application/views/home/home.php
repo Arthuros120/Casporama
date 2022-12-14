f567babffb66046ecee5f149c4f92147ac915fe9
Titouan GAUTIER <titoug@pop-os.localdomain>
2022-12-14 17:08:34 +0100

    home page v2 end

--- Casporama/application/views/home/home.php
+++ Casporama/application/views/home/home.php
@@ -1,41 +1,23 @@
 <!-- home/home -->
 
-<script>
-    document.body.onload=function() {
-        let nb_img = 3;
-        let pos = 0;
-
-        const container = document.getElementById("container")
-        const left = document.getElementById("left")
-        const right = document.getElementById("right")
-
-        container.style.width = 100 + "vw"
-        container.style.height = 68 + "vh"
-
-        for (let i=1; i<=nb_img; i++) {
-            div = document.createElement('div');
-            div.className="picture";
-            div.style.backgroundImage="url(<?= base_url() ?>static/image/casporama_home_"+i+".png)";
-            container.appendChild(div);
-        }
-    }
-</script>
-
-
 <div class="home">
     <nav class="home_nav">
         <div class="home_nav_left">
             <a><img src="<?= base_url()?>static/image/casporama.png"></a>
         </div>
         <div class="home_nav_right">
-            <a><h2>S'inscrire</h2></a>
-            <a><h2>Se connecter</h2></a>
+            <a href="<?= base_url() ?>User/register" ><h2>S'inscrire</h2></a>
+            <a href="<?= base_url() ?>User/login"><h2>Se connecter</h2></a>
         </div>
     </nav>
-    <div class="home_picture" id="carrousel">
-        <div id="container"></div>
-        <img id="left" class="home_picture_arrow_left" src="<?= base_url() ?>static/image/icon/arrow.svg">
-        <img id="right" class="home_picture_arrow_right" src="<?= base_url() ?>static/image/icon/arrow.svg">
+    <div id="carousel" >
+        <div id="container" ></div>
+        <button class="home_picture_arrow_left" id="left">
+            <img src="<?= base_url() ?>static/image/icon/arrow.svg" >
+        </button>
+        <button class="home_picture_arrow_right" id="right" >
+            <img src="<?= base_url() ?>static/image/icon/arrow.svg" >
+        </button>
     </div>
     <div class="home_cat">
         <div class="home_foot">
@@ -46,8 +28,7 @@
                 <div class="home_cat_title">
                     <h2>Football</h2>
                 </div>
-            </a>
-            
+            </a>  
         </div>
         <div class="home_volley">
             <a href="<?= base_url()?>Shop/home/Volleyball">
@@ -82,7 +63,12 @@
     </div>
 </div>
 
+<script>
 
-
+    document.body.onload=function() {
+        slider()
+    }
+    
+</script>
 
 <!-- home/home -->
