0b9685ffdc9650326d80feb59cb4ac86eb7f70f6
Arthur Hamelin <arthuros222@gmail.com>
2022-12-21 23:24:06 +0100

    finish countLines.py

--- src/countLines.py
+++ src/countLines.py
@@ -5,26 +5,55 @@ DEFAULT_PATH = "/var/www/html/sae_3/"
 LIST_FOLDER = ["GeneraTorCasporamaData/src/main", "sql/mariadb", "Casporama/static",
                 "Casporama/application/models", "Casporama/application/config",
                 "Casporama/application/controllers", "Casporama/application/views",
-                "Casporama/application/interfaces", "Casporama/application/models"]
+                "Casporama/application/interfaces", "Casporama/application/models",
+                "src"]
 
 """Recover all files in a folder"""
 def recover_files(path):
     
-    liste_fichier = []
+    list_files = []
     
-    for (_, _, fichiers) in walk(path):
+    for (repertoire, _, files) in walk(path):
         
-        liste_fichier.extend(fichiers)
+        for file in files:
+            
+            if not file.endswith((".png", ".webp", ".jpg", ".svg", ".ttf", ".ico", ".woff", ".woff2", ".eot", ".otf")):
+                
+                list_files.append(repertoire + "/" + file)
         
-    return liste_fichier
+    return list_files
 
+def count_lines(list_files):
+    
+    count = 0
+    
+    for file in list_files:
+        
+        with open(file, 'r') as f:
+            
+            lignes = f.readlines()
+            
+            count += len(lignes)
 
-print(recover_files(DEFAULT_PATH + LIST_FOLDER[0] + "/"))
+    return count
 
-
-# Liste = open(
-#     "/var/www/html/sae_3/Casporama/application/models/VerifyModel.php", 'r')
-
-# lignes = Liste.readlines()
-
-# print(len(lignes))
+if __name__ == "__main__":
+    
+    list_files = {}
+    
+    for folder in LIST_FOLDER:
+        
+        list_files[folder] = recover_files(DEFAULT_PATH + folder)
+    
+    
+    nbr_total_lines = 0
+    
+    for file in list_files:
+        
+        nbr_lines = count_lines(list_files[file])
+        
+        nbr_total_lines += nbr_lines
+    
+        print("Nombre de lignes dans le dossier {} : {} lignes.".format(file, nbr_lines))
+    
+    print("Nombre total de lignes du project: {} lignes.".format(nbr_total_lines))
