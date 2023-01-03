989ee87b8409b9775d351576cd4b7cee2a22a2f1
Luna MANSON <9223-E21A901C@users.noreply.gitlab.univ-nantes.fr>
2023-01-03 14:30:33 +0100

    Fin facture PDF

---
+++ Casporama/application/controllers/InvoicePDF.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once (APPPATH."libraries/pdf-invoice/src/InvoicePrinter.php");
+use Konekt\PdfInvoice;
+class InvoicePDF extends CI_Controller
+{
+    public function __construct()
+    {
+
+        parent::__construct();
+
+        $this->load->model('UserModel');
+        $this->load->model('InformationModel');
+        $this->load->model('LocationModel');
+        $this->load->model('ProductModel');
+        $this->load->model('OrderModel');
+    }
+
+
+    /**
+     * @param $idOrder
+     * @return void
+     */
+    public function getInvoice($idOrder) {
+        $invoice = new Konekt\PdfInvoice\InvoicePrinter("A4","€","en");
+
+        $this->UserModel->durabilityConnection();
+
+        /** @var OrderEntity $order
+         * @var UserEntity $user
+         * @var InformationEntity $billinginfo
+         * @var LocationEntity $locationinfo
+         */
+        $order = $this->OrderModel->findOrderById($idOrder);
+        $user  =  $this->UserModel->getUserById($order->getIduser());
+        $billinginfo = $this->InformationModel->getInformationByUserId($user->getId());
+        $locationinfo = $this->LocationModel->getLocationByUserId($user->getId(),$order->getIdlocation());
+        $productsid = explode(',',$order->getIdProducts());
+        $quantities = explode(',',$order->getQuantity());
+
+        $products = array();
+        foreach ($productsid as $productid) {
+            $products[] = $this->ProductModel->findById($productid);
+        }
+
+        $invoice->setLogo("static/image/casporama.png");
+        $invoice->setColor("#000000");
+        $invoice->setType("Facture d'achat");
+        $invoice->setReference($order->getIdorder());
+        $invoice->setDate("     ".$order->getDateorder());
+        $invoice->setFrom(array("Casporama","Casporama","Iut Nontes","44444") );
+        //var_dump( $locationinfo->getCodePostal() . $locationinfo->getCity() );
+        $invoice->setTo(array( ($billinginfo->getPrenom() ." ". $billinginfo->getNom()), (($billinginfo->getPrenom() ." " .$billinginfo->getNom())),
+           implode(" ",$locationinfo->getAdresse()) , $locationinfo->getCodePostal() ." ". $locationinfo->getCity()));
+        $total = 0;
+        for($i = 0; $i < sizeof($products);$i ++){
+            $keyi = strval($i);
+
+            $invoice->addItem($products[$keyi]->getName(),$products[$keyi]->getDescription(),$quantities[$keyi],0,
+                $products[$keyi]->getPrice(),0,$products[$keyi]->getPrice()*$quantities[$keyi]);
+            $total += $products[$keyi]->getPrice()*$quantities[$keyi];
+        }
+
+        $invoice->addTotal('Total TTC',$total);
+
+
+        $invoice->addParagraph("No item will be replaced or refunded if you don't have the invoice with you.");
+        $invoice->setFooternote("Casporama SA");
+
+        $invoice->render('Facture.pdf','I');
+
+
+
+    }
+
+
+}
--- Casporama/application/models/OrderModel.php
+++ Casporama/application/models/OrderModel.php
@@ -1,18 +1,32 @@
 <?php
 
-// * On importe les classes nécessaires
+require_once APPPATH . 'models/entity/ProductEntity.php';
+require_once APPPATH .'models/entity/OrderEntity.php';
+class OrderModel extends CI_Model
+{
 
-require_once APPPATH . 'models/entity/OrderEntity.php';
 
-/*
+    public function findOrderById($id): OrderEntity
+    {
 
-    * Class OrderModel
+        $queryOrder = $this->db->query("Call Orders.getOrderById(" . $id . ")");
+        $queryOrder->next_result();
 
-    * Cette classe permet de visualiser les commandes
+        $order = new OrderEntity() ;
+        $newrow = $queryOrder->row();
+        if ($newrow != null ) {
 
-*/
-class OrderModel extends CI_Model {
+            $order->setIdorder($newrow->idorder);
+            $order->setDateorder($newrow->dateorder);
+            $order->setIdproducts($newrow->idproduct);
+            $order->setQuantity($newrow->quantity);
+            $order->setIduser($newrow->iduser);
+            $order->setIdlocation($newrow->idlocation);
+            $order->setState($newrow->state);
 
-    
+        }
+        $queryOrder->free_result();
+        return $order;
 
+    }
 }
