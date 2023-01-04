1a78d619d4422ae99a3fd21de5859b3ce653a37f
Titouan GAUTIER <titouan.gautier@etu.univ-nantes.fr>
2023-01-04 14:02:50 +0100

    panier + admin

--- Casporama/application/views/admin/home/head.php
+++ Casporama/application/views/admin/home/head.php
@@ -1,3 +1,7 @@
 <!-- admin/home/head -->
 
+<link rel="stylesheet" type="text/css" href="<?php base_url() ?>../static/css/admin/home/content.css" >
+
+<title>Admin - Home</title>
+
 <!-- admin/home/head -->
---
+++ Casporama/application/views/cart/index/head.php
@@ -0,0 +1,7 @@
+<!-- cart/index/head -->
+
+<link type="text/css" rel="stylesheet" href="<?php base_url() ?>../static/css/cart/index/content.css" >
+
+<title>Panier</title>
+
+<!-- cart/index/head -->
