cc41fe1bb21925e6e8abe12416934a8369f8a44c
Titouan GAUTIER <titouan.gautier@etu.univ-nantes.fr>
2023-01-06 13:47:52 +0100

    add product

--- Casporama/application/views/templates/base.php
+++ Casporama/application/views/templates/base.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="<?php echo base_url() ?>static/css/global/color.css">
         <link rel="icon" type="image/svg" sizes="16x16" href="<?= base_url("static/image/icon/favicon.svg") ?>">
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/global/footer.css" >
-        <script type="text/javascript" src="<?= base_url()?>static/js/user/home/randomColor.js"></script>
+        <script type="text/javascript" src="<?= base_url()?>static/js/footer/randomColor.js"></script>
 
         <?= $loadView['head'] ?>
 
