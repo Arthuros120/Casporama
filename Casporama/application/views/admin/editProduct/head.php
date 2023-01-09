90e0520279f61d6645ed6da0e25eb1058f4146d3
Titouan GAUTIER <titouan.gautier@etu.univ-nantes.fr>
2023-01-09 11:41:55 +0100

    modify product + other things

--- Casporama/application/views/admin/editProduct/head.php
+++ Casporama/application/views/admin/editProduct/head.php
@@ -1,3 +1,7 @@
 <!-- admin/editProduct/head -->
 
+<link rel="stylesheet" href="<?= base_url() ?>static/css/admin/editProduct/content.css">
+
+<title>Modification Produit</title>
+
 <!-- admin/editProduct/head -->
