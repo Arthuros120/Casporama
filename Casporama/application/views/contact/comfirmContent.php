0aeba805a2bc89795fd45edadf9b6b81d36b20df
Titouan GAUTIER <titouan.gautier@etu.univ-nantes.fr>
2023-01-11 16:28:16 +0100

    contact success

--- Casporama/application/views/contact/comfirmContent.php
+++ Casporama/application/views/contact/comfirmContent.php
@@ -1,21 +1,31 @@
 <!-- contact/comfirmContent -->
 
+<style>
+    * {
+        --color0 :#D9D9D9;
+    }
+</style>
+
 <div class="confirm_contact">
 
     <div class="confirm_title">
         <h1> Bonjour <?= $firstname ?> </h1>
     </div>
 
+    <div class="confirm_content">
+        <p>Vous avez demandé à nous contacter par mail à l'adresse suivante : <span><?= $email ?></span> </p>
+        <p>Vous avez demandé à nous contacter pour l'object suivant : <span><?= $object ?></span> </p>
+        <p>Votre demande a bien été envoyé, nous vous répondrons dans les plus bref délais.</p>
+    </div>
+
+    <div class="confirm_return">
+        <a href="<?= base_url() ?>" >Retourner à l'accueil</a>
+    </div>
+
 </div>
 
 
 
-<p>Vous avez demandé à nous contacter par mail à l'adresse suivante : <?= $email ?> </p>
 
-<p>Vous avez demandé à nous contacter pour l'object suivant : <?= $object ?> </p>
-
-<p>Vous avez demandé à nous contacter avec le message suivant : <?= $message ?> </p>
-
-<p>Votre demande a bien été envoyé, nous vous répondrons dans les plus bref délais.</p>
 
 <!-- contact/comfirmContent -->
